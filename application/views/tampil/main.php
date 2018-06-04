f309c8a9bd4b3178282e27aea7966ef2e24e2fdb
Biem <Abiemassyikin@gmail.com>
2018-06-04 14:57:33 +0700

    siswa query

--- application/views/tampil/main.php
+++ application/views/tampil/main.php
@@ -302,14 +302,9 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Data Tables
-        <small>advanced tables</small>
+        PPDB AL AMIEN
+        <small>Yayasan Al Baitul Amien Jember</small>
       </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Tables</a></li>
-        <li class="active">Data tables</li>
-      </ol>
     </section>
 
     <!-- Main content -->
---
+++ application/views/tampil/siswa_v.php
@@ -0,0 +1,45 @@
+<div class="row">
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Data Siswa</h3><br><br>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <table id="example2" class="table table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>No</th>
+            <th>Nama Siswa</th>
+            <th>TTL</th>
+            <th>Jenis Kelamin</th>
+            <th>Nama Sekolah</th>
+            <th>Status Di terima</th>
+            <th>Aksi</th>
+          </tr>
+          </thead>
+          <tbody>
+            </tbody>
+            </table>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </div>
+  <!-- /.col -->
+</div>
+<!-- /.row -->
+<script type="text/javascript">
+  // function deleted(param){
+  //   var proc = window.confirm('Are you sure delete this data?');
+  //   if(proc){
+  //     document.location='<?php echo base_url(); ?>index.php/petugasdata/deletedata/'+param;
+  //   }
+  // }
+  // function updatejs(param){
+  //     document.location='<?php echo base_url(); ?>index.php/petugasdata/editData/'+param;
+  // }
+</script>
