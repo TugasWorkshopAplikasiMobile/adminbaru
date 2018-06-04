fb3ce5d7be6113654eeda8b567b06e407b4cb135
Biem <Abiemassyikin@gmail.com>
2018-06-04 15:11:09 +0700

    skin samping

--- application/controllers/siswa.php
+++ application/controllers/siswa.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class siswa extends CI_Controller {
 	public function index()
 	{	
-		$data['show']=$this->mymodel->selectwhere('admin',$dataa);
+		// $data['show']=$this->mymodel->selectwhere('admin',$dataa);
 		$data['side']='tampil/side';
 		$data['content']='tampil/siswa_v';
 		$this->load->view('tampil/main',$data);
