0017723d4e1fc0cbe59a3a243379355b322ae195
Christopher Nanda Jonathan <cnjonathan.cnj@gmail.com>
2018-06-06 11:24:48 +0700

    admindirektur parameter model

--- application/controllers/admin/admindirektur.php
+++ application/controllers/admin/admindirektur.php
@@ -11,7 +11,7 @@ class Admindirektur extends CI_Controller {
       $data= array(
   			'side'=>'tampil/side/sidedirektur',
   			'content'=>'tampil/siswa/siswa_v',
-  			'siswa'=>$this->mymodel->selectjoin());
+  			'siswa'=>$this->mymodel->selectjoin('jenjang.NAMA_JENJANG'));
   		$this->load->view('tampil/utama/main',$data);
   	}
   }
