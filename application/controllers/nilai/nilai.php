2c3c5f801a2da6ab365615c02adfaa93588417b3
bahrularif <bahrularif007@gmail.com>
2018-06-06 20:57:17 +0700

    aku bingung pie iki  per toper

--- application/controllers/nilai/nilai.php
+++ application/controllers/nilai/nilai.php
@@ -36,12 +36,41 @@ class Nilai extends CI_Controller {
 	  	}
 	}
 	public function update(){
-	$data= array(
-			'side'=>'tampil/side/side',
-			'content'=>'tampil/nilai/nilaiupdate',
-			'nilai'=>$this->mymodel->selectsiswafornilai());
-		$this->load->view('tampil/utama/main',$data);
+	$id=$this->uri->segment(4);
+	$data['dataupdate']= $this->db->query("SELECT * FROM admin WHERE ID_ADMIN='$id'");
+	$data['side']= 'tampil/side/side';
+			// 'side'=>'tampil/side/side',
+	$data['content']='tampil/nilai/nilaiupdate';
+			// 'content'=>'tampil/nilai/nilaiupdate',
+	$this->load->view('tampil/utama/main',$data);
+	'nilai'=>$this->mymodel->selectsiswafornilai());
+	$this->load->view('tampil/utama/main',$data);
 	}
+
+	// $id=$this->uri->segment(4);
+		// $data['dataupdate']= $this->db->query("SELECT * FROM admin WHERE ID_ADMIN='$id'");
+		// $data['side']= 'tampil/side/side';
+		// $data['content']='tampil/petugas/e-datapetugas';
+		// $this->load->view('tampil/utama/main',$data);
+
+	public function fungsiedit(){
+		header('Content-Type: application/json');
+		$id=$this->input->post('id');
+		// $where= array('no'=>$id);
+		$where= array('ID_SISWA'=>$id);
+		$data['NOMOR']=$this->input->post('no');
+		$data['NAMA_SISWA']=$this->input->post('nama');
+		$data['matematika']=$this->input->post('jk');
+		$data['bahasa_inggris']=$this->input->post('tlp');
+		$data['IPA ']=$this->input->post('sts');
+		$data['Bahasa_Indonesia']=$this->input->post('username');
+		$data['JUMLAH_NILAI_TEST']=$this->input->post('pass');
+		$data['RATA_RATA_NILAI_TEST']=$this->input->post('pass');
+		$this->mymodel->update('admin',$data,$where);
+		header('location:'.base_url().'petugas/petugasdata');
+	echo json_encode($data);	
+	}
+
 	public function tambahdatanilai(){
 	$data['side']= 'tampil/side/side';
 		$data['content']='tampil/nilai/tambahnilai';
