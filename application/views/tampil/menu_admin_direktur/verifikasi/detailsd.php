e6d6e51a549df3e7b852d2b617195b7b8956bb30
Christopher Nanda Jonathan <cnjonathan.cnj@gmail.com>
2018-06-25 21:15:21 +0700

    Ngerjakan Admin SD, SMP, Direktur

---
+++ application/views/tampil/menu_admin_direktur/verifikasi/detailsd.php
@@ -0,0 +1,11 @@
+<div class="col-md-6">
+  <div class="box box-primary">
+    <div class="box-header with-border">
+      <h3 class="box-title">Form Validasi SD</h3>
+    </div>
+
+     <img src="" class="img-rounded center-block" alt="Scan Pembayaran" width="304" height="236">
+
+  </div>
+
+</div>
